22bc26de81cdfee2c556a65d3327c29458ee7c65
daande01 <DANAND004@kunskapsforbundet.se>
2017-09-11 12:51:24 +0200

    test

--- artikel/artiklar.php
+++ artikel/artiklar.php
@@ -7,32 +7,32 @@
 
 <html lang="sv">
 <head>
-<meta charset="utf-8"/>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<meta charset="utf-8"/>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<style>
-article{
-	width:500px;
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<style>
+	article{
+		width:500px;
 
-	background-color:#dddddd;
-}
-fieldset{
+		background-color:#dddddd;
+	}
+	fieldset{
 
-	width:600px;
+		width:600px;
 
-}
-legend{
-	font-size:20px;
-}
+	}
+	legend{
+		font-size:20px;
+	}
 
 
-</style>
+	</style>
 
 
 
@@ -41,45 +41,44 @@ legend{
 
 
 <body>
-
 <fieldset>
-<legend>Artikelnamn</legend>
-<article>
-<div class="row">
-	<div class="col-sm-6">
-	<img src="bilder/bild1.jpg"/>
+		<legend>Artikelnamn</legend>
+		<article>
+			<div class="row">
+				<div class="col-sm-6">
+					<img src="bilder/bild1.jpg"/>
 
-	<br><br>
-<form>
+					<br><br>
+					<form>
 
-  <div class="form-group col-xs-5">
-    <label for="exampleSelect1">Antal</label>
-    <select class="form-control" id="exampleSelect1">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
-  </div>
+						<div class="form-group col-xs-5">
+							<label for="exampleSelect1">Antal</label>
+							<select class="form-control" id="exampleSelect1">
+								<option>1</option>
+								<option>2</option>
+								<option>3</option>
+								<option>4</option>
+								<option>5</option>
+							</select>
+						</div>
 
-  <button type="submit" class="btn btn-primary">Köp</button>
-</form>
+						<button type="submit" class="btn btn-primary">Köp</button>
+					</form>
+					
+					pris per styck:200kr
+				</div>
+				<div class="col-sm-6">
 
-	pris per styck:100kr
-	</div>
-	<div class="col-sm-6">
-
-Nya X1 Carbon har kolfiberförstärkt chassi och är precis lika tålig som föregångaren. Den har dessutom mindre format och är vår lättaste och tunnaste X1 Carbon någonsin. Ändå är den packad med mer kraft än någonsin: Windows 10 Pro, sjunde generationens Intel® Core™-processorer, blixtsnabb Thunderbolt 3 och 14-tums Quad-HD-bildskärm (Quad-HD endast tillgängligt på vissa modeller).
-
-
-	</div>
-</div>
+					Nya X1 Carbon har kolfiberförstärkt chassi och är precis lika tålig som föregångaren. Den har dessutom mindre format och är vår lättaste och tunnaste X1 Carbon någonsin. Ändå är den packad med mer kraft än någonsin: Windows 10 Pro, sjunde generationens Intel® Core™-processorer, blixtsnabb Thunderbolt 3 och 14-tums Quad-HD-bildskärm (Quad-HD endast tillgängligt på vissa modeller).
 
 
-</article>
+				</div>
+			</div>
 
-</fieldset>
+
+		</article>
+
+	</fieldset>
 
 
 </body>
